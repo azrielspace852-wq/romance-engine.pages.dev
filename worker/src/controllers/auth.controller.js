8e4cba5d8a54cef159ef46e2eabd241dc4c4863a
azrielspace852-wq <azrielspace852@gmail.com>
2026-08-28 15:17:19 +0700

    Delete worker directory

--- worker/src/controllers/auth.controller.js
+++
@@ -1,7 +0,0 @@
-import * as authService from '../services/auth.service.js';
-
-export async function verify(req) {
-  const { token } = req.body;
-  if (!token) throw new Error('Token required');
-  return authService.verify(token);
-}
